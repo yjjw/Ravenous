7058c884be07496f1cd52282548da10abcb4e12d
Jingwei Ye <yjjw97@gmail.com>
2020-06-24 23:27:24 -0700

    Interact with Yelp API

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,66 +1,30 @@
 import React from 'react';
-import logo from '../../logo.svg';
 import './App.css';
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
-import {render} from "react-dom";
-
-const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
-
-const businesses = [
-    business,
-    business,
-    business,
-    business,
-    business,
-    business
-];
+import Yelp from "../../util/Yelp";
 
 class App extends React.Component{
-    searchYelp(term, location, sortBy){
-        console.log('a message');
-        console.log(`${term}, ${location}, ${sortBy}`);
+    constructor(props) {
+        super(props);
+        this.state = {'businesses': []};
+        this.searchYelp = this.searchYelp.bind(this);
     }
-  render(){
-    return (
-        <div className="App">
-          <h1>ravenous</h1>
-          <SearchBar searchYelp={this.searchYelp} />
-          <BusinessList businesses={businesses} />
-        </div>
-    );
-  }
+    searchYelp(term, location, sortBy){
+        Yelp.search(term,location,sortBy)
+            .then(businesses => {
+                this.setState({'businesses': businesses});
+            });
+    }
+    render(){
+        return (
+            <div className="App">
+                <h1>ravenous</h1>
+                <SearchBar searchYelp={this.searchYelp} />
+                <BusinessList businesses={this.state.businesses} />
+            </div>
+        );
+    }
 }
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and say congratulations to your achievement so far.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
+
 export default App;
